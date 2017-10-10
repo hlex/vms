43dd586105e065c39090dc16959047b4f974429a
Mondit Thumniramon <mondit.thum@gmail.com>
2017-10-10 18:42:40 +0700

    Using EventItem to render

--- app/components/EventTitle/index.js
+++ app/components/EventTitle/index.js
@@ -1,41 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import cuid from 'cuid';
+
+import EventItem from '../EventItem';
 
 class EventTitle extends Component {
-
   static propTypes = {
     baseURL: PropTypes.string.isRequired,
     title: PropTypes.string,
-    bgImage: PropTypes.string,
-  }
+    item: PropTypes.shape({}),
+  };
 
   static defaultProps = {
     title: 'ซื้อเครื่องดื่ม/ขนม',
     bgImage: 'images/product-full.png',
-  }
+    item: {},
+  };
 
   render() {
-    const { title, baseURL, bgImage } = this.props;
+    const { title, baseURL, item } = this.props;
     return (
       <div className="promotion-title">
         <div className="_center">
           <h1>{title}</h1>
         </div>
         <div className="event-detail">
-          <div className="box">
-            <div className="ribbon-less ribbon-one-blue">
-              <p><span>เติมเงินฟรี<br />
-                5 บาท</span></p>
-            </div>
-            <div className="photo-item"><img src="images/product-1.png" />
-              <div className="price"><span>35</span> <b>฿</b></div>
-            </div>
-            <div className="requirements">
-              <p>1. ใส่ <span className="color-orange">เบอร์มือถือ <img className="icon middle-line" src="images/icon-phone.png" height="60" /></span></p>
-              <p>2. ชมโฆษณา 15 วินาที</p>
-              <p>3. <span className="color-red">เติมเงินฟรี 5 บาท</span> ทาง SMS</p>
-            </div>
-          </div>
+          <EventItem
+            key={cuid()}
+            baseURL={baseURL}
+            item={item}
+            handleClick={() => console.log('')}
+            hasButton={false}
+          />;
         </div>
       </div>
     );
