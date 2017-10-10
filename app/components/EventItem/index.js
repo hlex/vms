c54f026dcd3a529de245b924a4ea3b45bb7480e1
Mondit Thumniramon <mondit.thum@gmail.com>
2017-10-10 15:01:40 +0700

    Render EventItem Dynamic

--- app/components/EventItem/index.js
+++ app/components/EventItem/index.js
@@ -10,72 +10,141 @@ class EventItem extends Component {
     handleClick: PropTypes.func.isRequired,
   };
 
-  getRibbonColor = () => {
-    const rand = _.random(1, 6);
-    let color = '';
-    switch (rand) {
-      case 1:
-        color = 'red';
-        break;
-      case 2:
-        color = 'blue';
-        break;
-      case 3:
-        color = 'green';
-        break;
-      case 4:
-        color = 'yellow';
-        break;
-      case 5:
-        color = 'purple';
-        break;
-      case 6:
-        color = 'mint';
-        break;
-      default:
-        color = 'red';
-    }
-    return color;
-  };
+  convertToRibbonType = (ribbonType) => {
+    return ribbonType === 'A' ? 'one' : 'two';
+  }
 
-  getRibbonType = () => _.random(1, 2) === 1 ? 'one' : 'two';
+  convertToRibbonColor = (ribbonColor) => {
+    return ribbonColor.toLowerCase();
+  }
 
   renderRibbon = () => `${this.getRibbonType()}-${this.getRibbonColor()}`;
 
+  renderHowToParagraph = (howTo, order) => {
+    const { baseURL } = this.props;
+    const orderLabel = `${order}. `;
+    const shouldRenderIconPhone = howTo.indexOf('หมายเลข') >= 0 || howTo.indexOf('phone') >= 0;
+    const shouldRenderIconEmail = howTo.indexOf('อีเมล') >= 0 || howTo.indexOf('email') >= 0;
+    const shouldRenderIconLineId = howTo.indexOf('ไลน์') >= 0 || howTo.indexOf('LINE') >= 0;
+    const shouldRenderIconBarCode = howTo.indexOf('บาร์') >= 0 || howTo.indexOf('Bar') >= 0;
+    const shouldRenderIconQRCode = howTo.indexOf('คิวอาร์') >= 0 || howTo.indexOf('QR') >= 0;
+    const shouldRenderGetReward = howTo.indexOf('รับ') >= 0 || howTo.indexOf('Get') >= 0;
+    if (shouldRenderIconPhone) {
+      return (
+        <p>
+          {orderLabel}
+          <span className="color-orange">
+            {`${howTo} `}
+          </span>
+          <img
+            className="icon middle-line"
+            src={`${baseURL}/images/icon-phone.png`}
+            height="30"
+          />
+        </p>
+      );
+    } else if (shouldRenderIconEmail) {
+      return (
+        <p>
+          {orderLabel}
+          <span className="color-blue">
+            เบอร์มือถือ{' '}
+          </span>
+          <img
+            className="icon middle-line"
+            src={`${baseURL}/images/icon-phone.png`}
+            height="30"
+          />
+        </p>
+      );
+    } else if (shouldRenderIconLineId) {
+      return (
+        <p>
+          {orderLabel}
+          <span className="color-green">
+            เบอร์มือถือ{' '}
+          </span>
+          <img
+            className="icon middle-line"
+            src={`${baseURL}/images/icon-phone.png`}
+            height="30"
+          />
+        </p>
+      );
+    } else if (shouldRenderIconBarCode) {
+      return (
+        <p>
+          {orderLabel}
+          <span className="color-purple">
+            เบอร์มือถือ{' '}
+          </span>
+          <img
+            className="icon middle-line"
+            src={`${baseURL}/images/icon-phone.png`}
+            height="30"
+          />
+        </p>
+      );
+    } else if (shouldRenderIconQRCode) {
+      return (
+        <p>
+          {orderLabel}
+          <span className="color-pink">
+            เบอร์มือถือ{' '}
+          </span>
+          <img
+            className="icon middle-line"
+            src={`${baseURL}/images/icon-phone.png`}
+            height="30"
+          />
+        </p>
+      );
+    } else if (shouldRenderGetReward) {
+      return (
+        <p>
+          {orderLabel}<span className="color-red">{`${howTo}`}</span>
+        </p>
+      );
+    }
+    return (
+      <p>{`${orderLabel} ${howTo}`}</p>
+    );
+  }
+
   render() {
     const { baseURL, item, handleClick } = this.props;
-    // console.log('EventItem', this.props);
+    console.log('EventItem', this.props);
+
+    // ======================================================
+    // Ribbon
+    // ======================================================
+    const ribbonType = this.convertToRibbonType(item.tag.name);
+    const ribbonColor = this.convertToRibbonColor(item.tag.color);
+    const ribbonLabel = item.tag.label;
+    const ribbonValue = item.tag.value;
+    const ribbonUnit = item.tag.unit;
+
     return (
       <a className="box" key={cuid()} onClick={() => handleClick('/event/play', item)}>
-        <div className={`ribbon-less ribbon-${this.renderRibbon()}`}>
+        <div className={`ribbon-less ribbon-${ribbonType}-${ribbonColor}`}>
           <p>
             <span>
-              เติมเงินฟรี<br />5 บาท
+              {ribbonLabel}<br />{ribbonValue} {ribbonUnit}
             </span>
           </p>
         </div>
         <div className="photo-item">
-          <img src={`${baseURL}/images/product-1.png`} />
+          <img src={`${baseURL}/${item.productImage || ''}`} />
           <div className="price">
-            <span>35</span> <b>฿</b>
+            <span>{item.productPrice || ''}</span> <b>฿</b>
           </div>
         </div>
         <div className="requirements">
-          <p>
-            1. ใส่{' '}
-            <span className="color-orange">
-              เบอร์มือถือ{' '}
-              <img
-                className="icon middle-line"
-                src={`${baseURL}/images/icon-phone.png`}
-                height="30"
-              />
-            </span>
-          </p>
-          <p>2. ชมโฆษณา 15 วินาที</p>
-          <p>
-            3. <span className="color-red">เติมเงินฟรี 5 บาท</span> ทาง SMS
-          </p>
+          {
+            _.map(item.howTo, (instruction, index) => {
+              return this.renderHowToParagraph(instruction, index + 1);
+            })
+          }
           <div className="box-play-event">
             <span className="play-event">
               <i className="play">&nbsp;</i>เล่นกิจกรรมนี้
@@ -88,3 +157,52 @@ class EventItem extends Component {
 }
 
 export default EventItem;
+
+/*
+
+<p>
+  1. ใส่{' '}
+  <span className="color-orange">
+    เบอร์มือถือ{' '}
+    <img
+      className="icon middle-line"
+      src={`${baseURL}/images/icon-phone.png`}
+      height="30"
+    />
+  </span>
+</p>
+<p>2. ชมโฆษณา 15 วินาที</p>
+<p>
+  3. <span className="color-red">เติมเงินฟรี 5 บาท</span> ทาง SMS
+</p>
+
+getRibbonColor = () => {
+  const rand = _.random(1, 6);
+  let color = '';
+  switch (rand) {
+    case 1:
+      color = 'red';
+      break;
+    case 2:
+      color = 'blue';
+      break;
+    case 3:
+      color = 'green';
+      break;
+    case 4:
+      color = 'yellow';
+      break;
+    case 5:
+      color = 'purple';
+      break;
+    case 6:
+      color = 'mint';
+      break;
+    default:
+      color = 'red';
+  }
+  return color;
+};
+
+getRibbonType = () => _.random(1, 2) === 1 ? 'one' : 'two';
+*/
