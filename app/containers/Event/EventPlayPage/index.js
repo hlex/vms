9e6d8f74f8b316fd8d5b7b6e2ac9f181d5621fbb
Mondit Thumniramon <mondit.thum@gmail.com>
2017-10-14 04:38:16 +0700

    LineId UI Page

--- app/containers/Event/EventPlayPage/index.js
+++ app/containers/Event/EventPlayPage/index.js
@@ -6,17 +6,11 @@ import { connect } from 'react-redux';
 // ======================================================
 // Containers
 // ======================================================
-import {
-  FooterAction
-} from '../../Utils';
+import { FooterAction } from '../../Utils';
 // ======================================================
 // Components
 // ======================================================
-import {
-  Layout,
-  EventTitle,
-  InputWithPad,
-} from '../../../components';
+import { Layout, EventTitle, InputWithPad } from '../../../components';
 // ======================================================
 // Actions
 // ======================================================
@@ -32,103 +26,116 @@ import OrderSelectors from '../../../selectors/order';
 // ======================================================
 import { isEmpty } from '../../../helpers/global';
 
-const mapStateToProps = (state) => {
-  return {
-    baseURL: MasterappSelector.getBaseURL(state.masterapp),
-    selectedEvent: OrderSelectors.getSelectedEvent(state.order),
-    nextInput: OrderSelectors.getEventNextInput(state.order)
-  };
-};
+import lineId from '../../../images/line-id.png';
+
+const mapStateToProps = state => ({
+  baseURL: MasterappSelector.getBaseURL(state.masterapp),
+  selectedEvent: OrderSelectors.getSelectedEvent(state.order),
+  nextInput: OrderSelectors.getEventNextInput(state.order),
+});
 
 const actions = {
   ...ApplicationActions,
   ...Actions,
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(actions, dispatch);
-};
-
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
 class EventPlayPage extends Component {
-
   static propTypes = {
     baseURL: PropTypes.string.isRequired,
     selectedEvent: PropTypes.shape({}),
     nextInput: PropTypes.string,
     submitPlayEvent: PropTypes.func.isRequired,
     updateEventInput: PropTypes.func.isRequired,
-  }
+  };
 
   static defaultProps = {
     selectedEvent: {},
-    nextInput: ''
-  }
+    nextInput: '',
+  };
 
-  componentWillReceiveProps = (nextProps) => {
+  componentWillReceiveProps = nextProps => {
     const { submitPlayEvent } = this.props;
     if (isEmpty(nextProps.nextInput)) submitPlayEvent();
-  }
+  };
 
-  handleSubmitPage = (inputValue) => {
+  handleSubmitPage = inputValue => {
     const { nextInput, updateEventInput } = this.props;
     // ======================================================
     // Update nextInput
     // ======================================================
     updateEventInput(nextInput, inputValue);
-  }
+  };
 
-  renderInputMSISDN = () => {
-    return (
-      <div className="input-msisdn-box-wrapper">
-        <div className="input-msisdn-box">
-          <div className="content _center">
-            <h2>ใส่ เบอร์มือถือ ของคุณ</h2>
-            <p className="sm"><small>กรุณาตรวจสอบ หมายเลขโทรศัพท์ ให้ถูกต้อง</small></p>
-            <InputWithPad
-              type={'num'}
-              rules={{
-                required: 'กรุณาระบุหมายเลขโทรศัพท์',
-                mobileNumber: 'รูปแบบหมายเลขโทรศัพท์ไม่ถูกต้อง'
-              }}
-              onConfirm={this.handleSubmitPage}
-            />
-          </div>
+  renderInputMSISDN = () => (
+    <div className="input-msisdn-box-wrapper">
+      <div className="input-msisdn-box">
+        <div className="content _center">
+          <h2>ใส่ เบอร์มือถือ ของคุณ</h2>
+          <p className="sm">
+            <small>กรุณาตรวจสอบ หมายเลขโทรศัพท์ ให้ถูกต้อง</small>
+          </p>
+          <InputWithPad
+            type={'num'}
+            rules={{
+              required: 'กรุณาระบุหมายเลขโทรศัพท์',
+              mobileNumber: 'รูปแบบหมายเลขโทรศัพท์ไม่ถูกต้อง',
+            }}
+            onConfirm={this.handleSubmitPage}
+          />
         </div>
       </div>
+    </div>
     );
-  }
 
-  renderInputEmail = () => {
-    return (
-      <div className="input-msisdn-box-wrapper">
-        <div className="input-msisdn-box">
-          <div className="content _center">
-            <h2>ใส่ อีเมล ของคุณ</h2>
-            <p className="sm"><small>กรุณาตรวจสอบ อีเมล ให้ถูกต้อง</small></p>
-            <InputWithPad
-              type={'keyboard'}
-              rules={{
-                required: 'กรุณาระบุอีเมล',
-                email: 'รูปแบบอีเมลไม่ถูกต้อง'
-              }}
-              isFixed
-              onConfirm={this.handleSubmitPage}
-            />
-          </div>
+  renderInputEmail = () => (
+    <div className="input-msisdn-box-wrapper">
+      <div className="input-msisdn-box">
+        <div className="content _center">
+          <h2>ใส่ อีเมล ของคุณ</h2>
+          <p className="sm">
+            <small>กรุณาตรวจสอบ อีเมล ให้ถูกต้อง</small>
+          </p>
+          <InputWithPad
+            type={'keyboard'}
+            rules={{
+              required: 'กรุณาระบุอีเมล',
+              email: 'รูปแบบอีเมลไม่ถูกต้อง',
+            }}
+            isFixed
+            onConfirm={this.handleSubmitPage}
+          />
         </div>
       </div>
+    </div>
     );
-  }
 
-  renderInputUI = (inputType) => {
+  renderInputLineId = () => (
+    <div className="event-lineid-box">
+      <img alt="line" src={lineId} />
+      <div className="desc">
+        <h2>กรุณาแสกน LINE QR CODE</h2>
+        <p>เมื่อได้รับรหัสส่วนลดแล้ว กดปุ่มทำรายการต่อ</p>
+      </div>
+      <a className="button blue submit-button" onClick={() => console.log()}>
+        <p className="fade-flash">
+          ทำรายการต่อ{' '}
+        </p>
+      </a>
+    </div>
+    );
+
+  renderInputUI = inputType => {
     if (inputType === 'EMAIL') {
       return this.renderInputEmail();
     } else if (inputType === 'MSISDN') {
       return this.renderInputMSISDN();
+    } else if (inputType === 'LINE_ID') {
+      return this.renderInputLineId();
     }
     return '';
-  }
+  };
 
   render() {
     console.log(this);
@@ -137,16 +144,10 @@ class EventPlayPage extends Component {
     return (
       <div className="input-msisdn">
         <Layout.Title>
-          <EventTitle
-            title={'เล่นกิจกรรมรับส่วนลด'}
-            item={selectedEvent}
-            baseURL={baseURL}
-          />
+          <EventTitle title={'เล่นกิจกรรมรับส่วนลด'} item={selectedEvent} baseURL={baseURL} />
         </Layout.Title>
         <Layout.Content>
-          {
-            this.renderInputUI(nextInput)
-          }
+          {this.renderInputUI(nextInput)}
           <FooterAction />
         </Layout.Content>
       </div>
