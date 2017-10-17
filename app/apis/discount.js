94fcf013263aaab89ef75eb39aaea0837126e587
Mondit Thumniramon <mondit.thum@gmail.com>
2017-10-17 21:56:20 +0700

    use real response of service get discount (mockup response format)

--- app/apis/discount.js
+++ app/apis/discount.js
@@ -5,58 +5,52 @@ import { isVMSServiceError, convertVMSServiceResponseToError } from '../helpers/
 import URL from './url';
 
 export const serviceVerifyDiscountCode = code => {
-  // return new Promise((resolve, reject) => {
-  //   setTimeout(() => {
-  //     resolve(true);
-  //   }, 1000);
-  // });
   const data = {
     vtype: 'verifydiscountcode',
-    // code: '1111111111111',
     code,
   };
-  return fetchFacade(`${URL.verifyDiscount}${convertToURLParam(data)}`).then(() => {
-    const response = {
-      status: 'SUCCESSFUL',
-      'trx-id': '20171011215412',
-      'response-data': {
-        value: 3,
-        expireDate: '2017-12-01',
-      },
-      'process-instance': 'instance: 27.254.160.247:81',
-      version: '1.16.0',
-    };
+  // return fetchFacade(`${URL.verifyDiscount}${convertToURLParam(data)}`).then(() => {
+  return fetchFacade(`${URL.verifyDiscount}${convertToURLParam(data)}`).then((response) => {
+    let mockupResponse = {};
     console.log('serviceVerifyDiscountCode', response);
-    // const response = {
-    //   status: 'SUCCESSFUL',
-    //   'trx-id': '20171011215412',
-    //   fault: {
-    //     exception: 'DAOException',
-    //     'http-error-code': 200,
-    //     'http-error': 'Internal Server Error',
-    //     'technical-error-code': 'XXXXXX',
-    //     'technical-error': '',
-    //     'th-message': 'ไม่พบรหัสส่วนลดที่ระบุ',
-    //     'en-message': 'Cannot found discount code',
-    //     'technical-message': 'Cannot found discount code',
-    //   },
-    //   'process-instance': 'instance: 27.254.160.247:81',
-    //   version: '1.16.0',
-    // };
-    handleResponseCatchError(response, isVMSServiceError, convertVMSServiceResponseToError);
-    return extractResponseData(response);
+    if (_.get(response, '0.status', 1) === 1) {
+      // error
+      mockupResponse = {
+        status: 'SUCCESSFUL',
+        'trx-id': '20171011215412',
+        fault: {
+          exception: 'DAOException',
+          'http-error-code': 200,
+          'http-error': 'Internal Server Error',
+          'technical-error-code': 'XXXXXX',
+          'technical-error': '',
+          'th-message': 'ไม่พบรหัสส่วนลดที่ระบุ',
+          'en-message': 'Cannot found discount code',
+          'technical-message': 'Cannot found discount code',
+        },
+        'process-instance': 'instance: 27.254.160.247:81',
+        version: '1.16.0',
+      };
+    } else {
+      mockupResponse = {
+        status: 'SUCCESSFUL',
+        'trx-id': '20171011215412',
+        'response-data': {
+          value: 5,
+          expireDate: '2017-12-01',
+        },
+        'process-instance': 'instance: 27.254.160.247:81',
+        version: '1.16.0',
+      };
+    }
+    handleResponseCatchError(mockupResponse, isVMSServiceError, convertVMSServiceResponseToError);
+    return extractResponseData(mockupResponse);
   });
 };
 
 export const serviceUseDiscountCode = code => {
-  // return new Promise((resolve, reject) => {
-  //   setTimeout(() => {
-  //     resolve(true);
-  //   }, 1000);
-  // });
   const data = {
     vtype: 'usediscountcode',
-    // code: '1111111111111',
     code,
   };
   return fetchFacade(`${URL.verifyDiscount}${convertToURLParam(data)}`).then(response => {
