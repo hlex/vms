dcef23b0d2a1a505e87d2cad7d46daf8f6c5fa95
Mondit Thumniramon <mondit.thum@gmail.com>
2017-12-31 22:30:03 +0700

    Add ThankyouPage page to connect

---
+++ app/containers/ThankyouPage/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+// ======================================================
+// Hoc
+// ======================================================
+import { withAudio } from '../../hoc/withAudio';
+// ======================================================
+// Components
+// ======================================================
+import { Thankyou } from '../../components';
+// ======================================================
+// Selectors
+// ======================================================
+import MasterappSelector from '../../selectors/masterapp';
+// ======================================================
+// Actions
+// ======================================================
+const mapStateToProps = (state) => {
+  return {
+    baseURL: MasterappSelector.getBaseURL(state.masterapp),
+  };
+};
+
+const actions = {
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch);
+};
+
+
+class TopupProviderSelectionPage extends Component {
+
+  static propTypes = {
+    baseURL: PropTypes.string.isRequired,
+  }
+
+  render() {
+    const { baseURL } = this.props;
+    return (
+      <Thankyou baseURL={baseURL} />
+    );
+  }
+}
+
+export default withAudio({ src: 'http://localhost:8888/vms/html-v2/voice/5.m4a' })(connect(mapStateToProps, mapDispatchToProps)(TopupProviderSelectionPage));
