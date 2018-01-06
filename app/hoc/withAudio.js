c95da5add08ab43ca398ae2459a0a40b856f4a34
Mondit Thumniramon <mondit.thum@gmail.com>
2018-01-06 12:03:34 +0700

    Export with audio to default export and change others

--- app/hoc/withAudio.js
+++ app/hoc/withAudio.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
 // ======================================================
 // Components
 // ======================================================
 import { AudioPlayer } from '../components';
 
 const withAudio = ({ src }) => WrappedComponent => class ComponentWithAudio extends Component {
-  render = () => (
-    <div>
-      <AudioPlayer
-        src={src}
-        autoPlay
-        muted={false}
-        interval={5000}
-      />
-      <WrappedComponent
-        {...this.props}
-      />
-    </div>
-      );
-  };
+  state = {
+    isPlaying: true
+  }
 
-export default {
-  withAudio
+  componentWillMount = () => {
+    const { getState, subscribe } = this.context.store;
+    subscribe(() => this.switchLanguage(getState().language.currentActive));
+    this.switchLanguage(getState().language.currentActive);
+  }
+
+  render = () => {
+    console.log('withAudio', this)
+    const { isPlaying } = this.state;
+    return (
+      <div>
+        {
+          isPlaying &&
+          <AudioPlayer
+            src={src}
+            autoPlay
+            muted={false}
+            interval={5000}
+          />
+        }
+        <WrappedComponent
+          {...this.props}
+        />
+      </div>
+    );
+  };
 };
+
+export default withAudio;
