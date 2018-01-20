9681a31d3fc44c938d0011f0edfe966df8561768
Mondit Thumniramon <mondit.thum@gmail.com>
2018-01-21 00:53:16 +0700

    Refactor DevToolbar

---
+++ app/containers/DevToolbar/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as ApplicationActions from '../../actions/applicationActions';
+
+const mapStateToProps = state => state;
+
+const actions = {
+  ...ApplicationActions
+};
+
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+class DevToolbar extends Component {
+  render() {
+    const { backToHome, insertCoin, scanCode, openDoor, closeDoor } = this.props;
+    return (
+      <div className="development-toolbar">
+        <ul>
+          <li>
+            <a onClick={() => backToHome()}>HOME</a>
+          </li>
+          <li>
+            <a onClick={() => insertCoin(1)}>1B</a>
+          </li>
+          <li>
+            <a onClick={() => insertCoin(5)}>5B</a>
+          </li>
+          <li>
+            <a onClick={() => insertCoin(10)}>10B</a>
+          </li>
+          <li>
+            <a onClick={() => insertCoin(20)}>20B</a>
+          </li>
+          <li>
+            <a onClick={() => insertCoin(50)}>50B</a>
+          </li>
+          <li>
+            <a onClick={() => insertCoin(100)}>100B</a>
+          </li>
+          <li>
+            <a onClick={() => scanCode('Q12345R')}>QR</a>
+          </li>
+          <li>
+            <a onClick={() => scanCode('2048486920593')}>BC</a>
+          </li>
+          <li>
+            <a onClick={() => scanCode('http://line.me/th/q/_hlexpond')}>LINE</a>
+          </li>
+        </ul>
+        <br />
+        <ul>
+          <li>
+            <a onClick={() => openDoor()}>OPEN DOOR</a>
+          </li>
+          <li>
+            <a onClick={() => closeDoor()}>CLOSE DOOR</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+DevToolbar.propTypes = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DevToolbar);
